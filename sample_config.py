a166e14194b045821b691e30054fbeb9625c089b
Nick Henderson <nick@nihenderson.com>
2018-03-14 16:04:27 -0700

    added sample config

---
+++ sample_config.py
@@ -0,0 +1,15 @@
+# Reddit configs
+
+reddit_id = ''
+reddit_secret = ''
+reddit_username = ''
+reddit_password = ''
+user_agent = '{Your Bot Name} by /u/{Your reddit Name}'
+
+# Spotipy configs
+spotify_username = ''
+scopes = 'playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify-private'
+spotify_client_id = ''
+spotify_client_secret = ''
+redirect_uri = 'https://localhost/'
+
