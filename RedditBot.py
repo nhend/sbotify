fda5c2d3c0f5b893d70693cbe4208e9279e268ff
Nick Henderson <nick@nihenderson.com>
2018-03-15 10:41:18 -0700

    improved documentation

--- RedditBot.py
+++ RedditBot.py
@@ -1,7 +1,7 @@
 import Sbotify as sbot
 from spotipy import SpotifyException
 import praw
-from praw.models import Comment
+from praw.models import Comment, Message
 import config
 import time
 import re
@@ -11,17 +11,12 @@ client_secret = config.reddit_secret
 username = config.reddit_username
 password = config.reddit_password
 user_agent = config.user_agent
-hot_word = config.hot_word
-subreddit_list = config.subreddits
 
-#FORMAT: !Sbotify song:94 Dreaming artist:ATO
+#FORMAT: /u/Sbotify **94 Dreaming**:*ATO* **Somebody to Love**:*Abhi the Nomad*
 #IMPLEMENT:
-#    Allowing for just a band name (pick 1 popular song)
-#    Allowing for just a song name ???
-
-#FIX:
 #    Delete comments with bad score (<= -2 ?)
-#    Convert errors to error response (incorrect format etc.)
+#FIX:
+#
 
 reddit = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
@@ -29,52 +24,54 @@ reddit = praw.Reddit(client_id=client_id,
                      user_agent=user_agent,
                      username=username)
 
-subreddits = reddit.subreddit('+'.join(subreddit_list))
-
-
 def parse_comment(comment):
     
-    '''
-    if #can't find song or artist#
-        comment.reply("Hi! Looks like you've used the wrong format, in case you've forgotten it's" + \
-                      "\n\n!Sbotify song: {song title} artist: {artist name}" + \
-                      "\n\n*** \n\n ^^I'm ^^a ^^bot ^^in ^^development ^^by ^^/u/CarpetStore. [^^Message ^^him](https://www.reddit.com/message/compose/?to=CarpetStore) ^^with ^^comments ^^or ^^complaints.")
-        
-        print('Posted error reply to ' + comment.id)
-    '''
-    titles_list = re.findall(r'\*\*(.*?)\*\*:\*(.*?)\*',comment.body)
-
-    '''
-    for pair in titles_list:
-        print(pair[0] + ' ' + pair[1])
-    '''
+    # Uses regex to separate data found in comment to song-artist pairs
+    titles_list = re.findall(r'\*\*(.*?)\*\*(?: ?):(?: ?)\*(.*?)\*',comment.body)
     
-    #  titles_list looks like [(Sex and Drugs, Abhi the Nomad), (Pusher, Alt-J)]
+    # titles_list looks like [(Sex and Drugs, Abhi the Nomad), (Pusher, Alt-J)]
     return titles_list
     
 def gen_title(comment):
     
-    #playlist title format is: [POST ID] POST TITLE
-    #ex. [t3_827zrc] Favorite Count Basie songs?
+    # Playlist title format is: [POST ID] POST TITLE
+    # ex. [t3_827zrc] Favorite Count Basie songs?
       
     title = '[' + get_link_id(comment) + '] ' + comment.link_title
-    #Spotify only allows 50 character titles, so limit
+    # Spotify only allows 50 character titles, so limit to 50 if necessary
     title = (title[:50]) if len(title) > 50 else title
     
     return title
 
 def gen_desc(comment):
     
-    #description format is: POST TITLE by USER in SUBREDDIT
+    # Playlist description format is: "POST TITLE" in r/SUBREDDIT
     
     desc = '\"' + comment.link_title + '" in ' + comment.subreddit_name_prefixed
-    #Spotify only allows 300 character descriptions, so limit
+    # Spotify only allows 300 character descriptions, so limit
     desc = (desc[:300]) if len(desc) > 300 else desc
     
     return desc
 
 def get_link_id(comment):
-    #print(comment)
+    '''Gets the id of the thread from a comment
+    
+    Since we're getting summons from the inbox instead of a subreddit comment stream,
+    we can't use parent_id or link_id (if a comment was a child of another comment,
+    parent_id would return the id of its parent comment, not the thread id)
+    
+    Instead, we get the link from either the 'context' (if comment object is a parent
+    comment) or the 'permalink' (if comment object is a child comment) vars, and then
+    extract the link id from the link using regex
+
+    This function wouldn't be necessary if the Comment object vars were more consistent.
+    
+    Returns: 
+        A string
+        
+        Example:
+        84ar17
+    '''
     
     vars_dict = vars(comment)
     if 'context' in vars_dict:
@@ -87,40 +84,63 @@ def get_link_id(comment):
         
     title = re.findall(r'/comments/(.*?)/',vars_dict[location])
 
-    return title[0]  # findall() returns an array so just take the first one
+    return title[0]  # findall() returns an array, we just need the first element
 
 def add_song(comment):
+    '''Adds a song to an existing or new playlist
     
+    Uses a list of link ids (link_ids.txt) to keep track of which threads
+    have already had a summon in them. In other words, which threads have
+    an existing playlist.
+    
+    If the thread's id is not found in the list, we record the link id in 
+    the list, create a playlist, and add the user's songs. If the id is 
+    found, just add the user's songs. 
+    
+    Returns:
+        A list - track_ids
+        
+        track_ids[0] is a list of the ids of the requested tracks
+        track_ids[1] is the playlist id we're adding the tracks to
+        
+        Example:
+        [['0ZAbupc7jAQpG9IxojQ3s3', '6ZljnCVxgY8UKsqs1urMMc'], '4O8Fn7JHyhnpL1jVNrw2GK']
+    '''
     track_ids = []
-    #if comment.thread_id in link_ids.txt, add song to playlist of that id
+    
     with open('link_ids.txt', 'a+') as link_ids:
             link_ids.seek(0)
             
             while True:
-                if get_link_id(comment) in link_ids.read(): #if we've seen this thread before, just add the song to that id playlist
-                    #add song
+                # If we've seen this thread before, just add the song to this thread's playlist
+                if get_link_id(comment) in link_ids.read(): 
+                    
                     name_pairs = parse_comment(comment)
 
                     for pair in name_pairs:
+                        # Use Spotify's search function to get id of the requested song
                         track_id = sbot.search_track(pair[0], pair[1])
                         playlist_id = sbot.get_playlist_id(get_link_id(comment))
                     
+                        # Add the song and record 
                         sbot.add_track(track_id, playlist_id)
                         track_ids.append(track_id)
                     break
                 
-                else: #else, create a playlist and add the song
+                else: # If we haven't, create a playlist and add the song
+                    # Add thread's id to link_ids.txt
                     link_ids.write('\n' + get_link_id(comment))
-                    #create playlist                   
+                    # Create playlist with gen_title() and gen_desc()                
                     sbot.create_playlist(gen_title(comment), gen_desc(comment))
                     
-                    #add song
                     name_pairs = parse_comment(comment)
 
                     for pair in name_pairs:
+                        # Use Spotify's search function to get id of the requested song
                         track_id = sbot.search_track(pair[0], pair[1])
                         playlist_id = sbot.get_playlist_id(get_link_id(comment))
-                    
+                        
+                        # Add the song and record 
                         sbot.add_track(track_id, playlist_id)
                         track_ids.append(track_id)                      
                     break
@@ -128,89 +148,138 @@ def add_song(comment):
     return [track_ids, playlist_id]
  
 def post_comment(comment, track_ids):
+    '''Reply to summoner with their songs and other info
     
-    #track_ids looks like [['2384thk3j4thh938h', 'i732rhwr7befw23r'], '39485y934gfwkdfg29']
-    #so, songs are track_ids[0][i] and playlist is track_ids[1][0]
+    Supplies the summoner with the name of the thread's playlist, a link 
+    to the thread's playlist, the name and artist name of each requested 
+    song, a sample of each song (or a link, if no sample is available), 
+    a link to the bot's FAQ, a link to message me, and a link to delete 
+    this comment and remove their songs from the thread's playlist.
     
-    reply = "I've added your songs to this thread's playlist: ['" + sbot.get_playlist_name(get_link_id(comment)) + "'](" + sbot.get_playlist_link(get_link_id(comment)) + ")"   
+    In order to supply a delete link, we need the id of the comment we're 
+    posting with this call. So we post the comment, get the id of the 
+    comment, and immediately edit it to include the deletion link.
+    '''
     
-    counter = 0
+    reply = "I've added your songs to this thread's playlist: ['" + sbot.get_playlist_name(get_link_id(comment)) \
+        + "'](" + sbot.get_playlist_link(get_link_id(comment)) + ")"   
     
-    for track_id in track_ids[0]:    
-        if sbot.get_track_preview(track_ids[0][counter]) != None:
-            reply += "\n\n [Sample of **" + sbot.get_track_names(track_ids[0][counter])[0] + "** by *" + sbot.get_track_names(track_ids[0][counter])[1] + "*](" + sbot.get_track_preview(track_ids[0][counter]) + ")"
-        else:
-            reply += "\n\n **" + sbot.get_track_names(track_ids[0][counter])[0] + "** by *" + sbot.get_track_names(track_ids[0][counter])[1] + "* doesn't have a sample available. [Link to the full song](" + sbot.get_track_link(track_ids[0][counter]) + ")"
+    # Track_ids looks like [['0ZAbupc7jAQpG9IxojQ3s3', '6ZljnCVxgY8UKsqs1urMMc'], 
+    #    '4O8Fn7JHyhnpL1jVNrw2GK']
+    # So, a song id is track_ids[0][i] and playlist is track_ids[1][0]
+    
+    for track_id in track_ids[0]:   
         
-        counter += 1
+        # If the song has a preview, add it to the reply 
+        if sbot.get_track_preview(track_id) != None:
+            reply += "\n\n [Sample of **" + sbot.get_track_names(track_id)[0] + "** by *" \
+                 + sbot.get_track_names(track_id)[1] + "*](" \
+                 + sbot.get_track_preview(track_id) + ")"
+                 
+        # If it doesn't, add the link of the full song
+        else:
+            reply += "\n\n **" + sbot.get_track_names(track_id)[0] + "** by *" \
+                + sbot.get_track_names(track_id)[1] \
+                + "* doesn't have a sample available. [Link to the full song](" \
+                + sbot.get_track_link(track_id) + ")"
     
-    reply += "\n\n*** \n\n ^^I'm ^^a ^^bot ^^in ^^development ^^by ^^/u/CarpetStore. [^^Message ^^him](https://www.reddit.com/message/compose/?to=CarpetStore) ^^with ^^comments ^^or ^^complaints." 
+    reply += "\n\n*** \n\n[^\[FAQ\]](https://www.reddit.com/r/Sbotify/comments/84ic13/sbotify_info/)" \
+        + "^- [^Message ^my ^developer](https://www.reddit.com/message/compose/?to=CarpetStore) ^-" 
+    
+    # Post comment before the deletion link edit
     posted_comment = comment.reply(reply)
+     
+    delete_link = " [^Delete ^this ^comment ^and ^remove ^your ^songs]" \
+        + "(https://www.reddit.com/message/compose/?to=Sbotify&subject=delete&message=" + posted_comment.id + ")"
     
-    delete_link = " ^^Did ^^I ^^get ^^it ^^wrong? [^^Click ^^here ^^to ^^delete ^^this ^^comment ^^and ^^remove ^^your ^^song.](https://www.reddit.com/message/compose/?to=Sbotify&subject=delete&message=" \
-    + posted_comment.id + ")"
-    
+    # Since the edit requires a second API request immediately after, give the API some time to breathe
     time.sleep(1)
+    
+    # Edit the comment with deletion link
     posted_comment.edit(posted_comment.body + delete_link)
     
     print('Posted reply to ' + comment.id)
     
-def check_delete():
+def delete_comment(message):
+    '''Allows the user to delete a comment by sending a PM
     
-    for message in reddit.inbox.messages(limit=25): 
-        #using python short circuit evaluation to make sure the message is new, about deletion, about an actual comment, and sent by the author of the relevant comment
-        if message.new and message.subject.lower() == 'delete':
-            print('Found delete message')
+    When the user clicks the deletion link to remove Sbotify's reply and
+    their requested songs from the thread's playlist, a PM is generated
+    containing the comment id of Sbotify's reply. 
+    
+    Before deleting, we need to make sure that the id actually belongs 
+    to one of our comments and that the person sending the PM is the 
+    owner of the comment we replied to. 
+    '''
+    
+    my_comment = reddit.comment(id=message.body)
+    
+    # Parent_id includes prefix 't1_', but comment object doesn't take fullname, just id (stuff after prefix)
+    parent_comment = reddit.comment(id=my_comment.parent_id[3:])
+    
+    # Check that the id is actually a comment we've posted, that the PM sender owns the parent comment
+    if parent_comment.id in open('comment_ids.txt').read() and \
+        message.author.name == parent_comment.author:
             
-            my_comment = reddit.comment(id=message.body)
+            # To remove a song, we need the track id and the playlist id. To get the track id we 
+            # need the track names, and to get the playlist id we need the link id. Both of these 
+            # can be found with a Comment object.
+            track_titles = parse_comment(parent_comment)
+            playlist_id = sbot.get_playlist_id(get_link_id(parent_comment))
             
-            parent_comment = reddit.comment(id=my_comment.parent_id[3:]) #parent_id includes prefix 't1_', but comment object doesn't take fullname, just id (stuff after prefix)
+            # Iterate over the user's requested tracks and remove each one from the playlist
+            # Uses track_title[0] = song name and track_title[1] = artist name
+            for track_title in track_titles:
+                track_id = sbot.search_track(track_title[0], track_title[1])
+                sbot.remove_track(track_id, playlist_id)
             
-            if parent_comment.id in open('comment_ids.txt').read(): 
-                if message.author.name == parent_comment.author:
-            
-                    track_titles = parse_comment(parent_comment)
-                    playlist_id = sbot.get_playlist_id(get_link_id(parent_comment))
-                    
-                    for track_title in track_titles:
-                        track_id = sbot.search_track(track_title[0], track_title[1])
-                        sbot.remove_track(track_id, playlist_id)
-                    
-                    my_comment.delete()               
-                    message.mark_read()
-                    
-                    print('Deleted ' + my_comment.id + ' on: ' + parent_comment.id)   
-        #else break
+            # Delete comment and remove the message from the unread queue
+            my_comment.delete()               
+            message.mark_read()
+        
+            print('Deleted ' + my_comment.id + ' on: ' + parent_comment.id)          
 
 def main():
     
-    for comment in reddit.inbox.unread():
-        if isinstance(comment, Comment) and bool(re.search('\*\*(.*?)\*\*:\*(.*?)\*', comment.body)):
-            #check duplicates against file of replied IDs
+    for item in reddit.inbox.unread():
+        # Since Messages or Comments can show up in the inbox, we need to filter for each. Also, users
+        # can mention the bot without needing a song, so filter those out by looking for our format
+        if isinstance(item, Comment) and bool(re.search('\*\*(.*?)\*\*(?: ?):(?: ?)\*(.*?)\*', item.body)):
+            comment = item  # Rename for clarity
+            
+            # Check duplicates against file of ids we've already replied to/seen
             with open('comment_ids.txt', 'a+') as comment_ids:
                 comment_ids.seek(0)
                 
                 while True:
-                    if comment.id in comment_ids.read(): #if repeat comment, ignore
+                    if comment.id in comment_ids.read(): # If repeat comment, ignore
                         break
-                    else: #else, add song to thread's playlist
+                    else: # Else, record id and add requested songs to thread's playlist
                         comment_ids.write('\n' + comment.id)
-                        sbot.refresh() #checks if new API token is needed, refreshes if so
                         
-                        comment.mark_read()
+                        # refresh() actually checks if the Spotify API token is expired,
+                        # we should still only call it when a Spotify action is needed
+                        # (i.e. when we find a comment with a song request)
+                        sbot.refresh() 
+                        
+                        comment.mark_read() # Not actually necessary but makes the inbox cleaner
                         track_ids = add_song(comment)
                         post_comment(comment, track_ids)
                         
                         break
-    
-    check_delete()
-    #refresh()
+        
+        # Not all messages will be deletion messages so we need to filter for those            
+        if isinstance(item, Message) and item.subject.lower() == 'delete':
+            sbot.refresh()
+            delete_comment(item)
                 
 if __name__ == '__main__':
     while True:
         try:
             main()
         
+        # Most issues are caught before Spotify is involved, so usually the problem
+        # is the API token being expired
         except SpotifyException:
             sbot.refresh()
             print('Refreshed token')
@@ -219,6 +288,7 @@ if __name__ == '__main__':
             print('Goodbye!')
             break
         
+        # It's lazy but necessary to keep the bot running 24/7
         except Exception as e:
             print('Exception occurred:')
             reddit.redditor('CarpetStore').message('Sbotify Error occurred', e)
